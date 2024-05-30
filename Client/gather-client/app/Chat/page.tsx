c0ddac637f1d08b9e4348d0472c63919260e7fb1
Fengyzh <fewoodse@gmail.com>
2024-05-30 01:57:55 -0400

    chat page styling

--- Client/gather-client/app/Chat/page.tsx
+++ Client/gather-client/app/Chat/page.tsx
@@ -4,12 +4,84 @@ import './chat.css'
 export default function page() {
   return (
     <div className='chat-page'>
+      <div className='chat-page-title-cont'>
+        <h2 className='chat-page-title'> Chat Page </h2>
+      </div>
 
-      <div className='chat-box'> Hello </div>
+      <div className='chat-box'> 
+        <div className='chat-bubble chat-client'>hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quos blanditiis magnam aspernatur autem amet molestias, accusamus placeat nobis iusto aliquid, ducimus recusandae exercitationem fuga, dicta repellat. Error praesentium nemo enim, voluptas tempora accusantium quo repudiandae fuga? Minus, error illo suscipit ut voluptatem sunt accusamus! Eum quo eaque iure, dolorum natus libero unde commodi! Soluta sapiente quasi ducimus eum! Accusantium!</div>
+        <div className='chat-bubble chat-ai'>hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quos blanditiis magnam aspernatur autem amet molestias, accusamus placeat nobis iusto aliquid, ducimus recusandae exercitationem fuga, dicta repellat. Error praesentium nemo enim, voluptas tempora accusantium quo repudiandae fuga? Minus, error illo suscipit ut voluptatem sunt accusamus! Eum quo eaque iure, dolorum natus libero unde commodi! Soluta sapiente quasi ducimus eum! Accusantium!</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>hi</div>
+        <div>bye</div>
+
+
+        <div className='chat-space'></div>
+      </div>
+
 
       <div className='chatbox-cont'>
         <input className='chat-input' type='text'></input>
-        <button className='chat-send'>Send</button>
+        <button className='chat-send'> {'>'} </button>
       </div>
 
     </div>
